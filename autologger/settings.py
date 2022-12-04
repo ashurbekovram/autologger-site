5501bc69d6d49825fde586850b93f23a03a96eaa
Ramazan Ashurbekov <ashurbekov.ram@gmail.com>
2022-12-04 12:10:32 +0300

    Disable debug mode

--- autologger/settings.py
+++ autologger/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kouyavo#o4hpze3s1g4if93(np6*p*#asss-k4&(j015xd+n!h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
