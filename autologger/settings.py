5436ad7e2f15c5d3510f74de6bd4e4e18ebb20ce
Ramazan Ashurbekov <ashurbekov.ram@gmail.com>
2022-12-04 11:51:54 +0300

    Update settings.py for static and media dirs

--- autologger/settings.py
+++ autologger/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = Path(BASE_DIR / 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path(BASE_DIR / 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
