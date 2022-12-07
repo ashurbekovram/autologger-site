51b09eb1c713ace1764d2b681a864220519101b0
Ramazan Ashurbekov <ashurbekov.ram@gmail.com>
2022-12-08 01:14:35 +0300

    Admin panel update for new models

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -12,5 +12,20 @@ class BrandAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'name')
 
 
+class SeriesAdmin(admin.ModelAdmin):
+    list_display = ('id', '__str__')
+    list_display_links = ('id', '__str__',)
+    ordering = ('brand', 'name')
+
+
+class GenerationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'series', 'start_year', 'end_year')
+    list_display_links = ('id', 'name')
+    list_filter = ('series',)
+    ordering = ('series', 'start_year')
+
+
 admin.site.register(Country, CountryAdmin)
 admin.site.register(Brand, BrandAdmin)
+admin.site.register(Series, SeriesAdmin)
+admin.site.register(Generation, GenerationAdmin)
